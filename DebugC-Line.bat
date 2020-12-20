0852820647721db5abd9d629319c5c1d18bfb659
ZorritoYT <69325535+ZorritoYT@users.noreply.github.com>
2020-12-20 12:37:55 -0400

    Update DebugC-Line.bat

--- DebugC-Line.bat
+++ DebugC-Line.bat
@@ -1,5 +1,5 @@
 @echo off & color a
 cls
 set/p cLINE=< cLINES.ZQ
-for /l %%a in (1,1,%cLINE%) do (echo %%a > lineCFG.ZQ & timeout 4 /nobreak >nul)
+for /l %%a in (1,1,%cLINE%) do (echo %%a > lineCFG.ZQ & timeout 2 /nobreak >nul)
 exit
